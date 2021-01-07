376a14949967656d1647b0313a58fa2b5226c053
Kwaku Sarpong Manu <owurakwakumanu@gmail.com>
2021-01-07 00:48:08 +0000

    main types of useful neural networks

--- agent/agent.js
+++ agent/agent.js
@@ -1,10 +1,22 @@
-// NN here
-const brain = require('brain.js');
+const brain = require('brain.js');      // create supporting environment
 
-// const net = new brain.NeuralNetwork();
 // Neuural net types:
+// Recurrent Neural Network
 // RNNTimeStep
 // GRUTimeStep
 // LSTMTimeStep
 
-const net = new brain.recurrent.RNNTimeStep();
+exports.RNN = function(config) {
+    return new brain.NeuralNetwork(config);
+}
+exports.RNNTimeStep = function(config){
+    return new brain.recurrent.RNNTimeStep(config);
+}
+
+exports.GRUTimeStep = function(config){
+    return new brain.recurrent.GRUTimeStep(config);
+}
+
+exports.LSTMTimeStep = function(config){
+    return new brain.recurrent.LSTMTimeStep(config);
+}
