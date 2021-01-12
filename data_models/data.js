a48c5208c5d4f8c24f2623ef9c35be1a55d90954
Kwaku Sarpong Manu <owurakwakumanu@gmail.com>
2021-01-12 22:08:50 +0000

    additional data models

--- data_models/data.js
+++ data_models/data.js
@@ -1,4 +1,5 @@
-exports.OHLV_model = function() {
+exports.OHLV_model = function () {
+    
     return {
         input:
         {
@@ -11,5 +12,46 @@ exports.OHLV_model = function() {
         output:{
             Close:0.00,
         }
+    }
+}
+
+exports.Moving_Average_Model = function () {
+    
+    return {
+        input:{
+            Open: 0.00,
+            High: 0.00,
+            Low: 0.00,
+            SMA_20: 0.00,
+            SMA_50: 0.00,
+            SMA_100: 0.00,
+            SMA_200: 0.00,
+            Volume:0.00,
+        },
+        output:{
+            Close: 0.00
+        }
+        
+    }
+}
+
+exports.Moving_Average_Model_beta = function () {
+    
+    return {
+        input:{
+            Open: 0.00,
+            High: 0.00,
+            Low: 0.00,
+            SMA_20: 0.00,
+            SMA_50: 0.00,
+            SMA_100: 0.00,
+            SMA_200: 0.00,
+            Volume:0.00,
+            Close: 0.00,
+        },
+        output:{
+            Next_Close: 0.00
+        }
+        
     }
 }
