460564564d320dd158591e04ea65fde42eb015f6
Kwaku Sarpong Manu <owurakwakumanu@gmail.com>
2021-01-12 22:06:34 +0000

    minor changes

--- agent/agent.js
+++ agent/agent.js
@@ -20,3 +20,34 @@ exports.GRUTimeStep = function(config){
 exports.LSTMTimeStep = function(config){
     return new brain.recurrent.LSTMTimeStep(config);
 }
+
+class Agent {
+    // Agent();
+    net;
+    config = {};
+
+    setConfigs(configs) {
+        config = configs
+    }
+
+    setNetType(type){
+        switch(type){
+            case 'RNN':
+                net = brain.recurrent.RNNTimeStep;
+            case 'LSTM':
+                net = brain.recurrent.LSTMTimeStep;
+            case 'GRU':
+                net = brain.recurrent.GRUTimeStep;
+            default:
+                net = brain.recurrent.RNNTimeStep;
+        }
+    }
+
+    trainNet(data){
+        this.net.train(data, this.config);
+    }
+
+    runNet(data){
+        return this.net.run(data);
+    }
+}
